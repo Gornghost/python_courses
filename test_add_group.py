9ad4dc4fca20625220b22748f6262a38d1d88263
Yevhen Hornysh <e.gornysh@gmail.com>
2020-04-13 18:16:21 +0300

    creating fixtures, moving from unittest to pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,70 +1,22 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_xpath("// a[contains(text(), 'LOGOUT')]").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("GROUPS").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//body//input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="test group name", header="test group header", footer="test group footer"))
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def tearDown(self):
-        self.wd.quit()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="test group name", header="test group header", footer="test group footer"))
+    app.logout()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
