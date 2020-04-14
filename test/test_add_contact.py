939e22e5a6021cc0d377917756e2e522725b9623
Yevhen Hornysh <e.gornysh@gmail.com>
2020-04-14 12:10:23 +0300

    created class SessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="test fname", middle_name="test mname", last_name="test lname", nickname="test nickname",
                                     title="test title", company="test company", address="test address",
                                     phone_home="test phone home", phone_mobile="test phone mobile", phone_work="test phone work", fax="test fax",
@@ -19,4 +19,4 @@ def test_add_contact(app):
                                     birthday_day="//select[@name='bday']//option[3]", birthday_month="//select[@name='bmonth']//option[5]", birthday_year="2000",
                                     anniversary_day="//select[@name='aday']//option[3]", anniversary_month="//select[@name='amonth']//option[3]", anniversary_year="2000",
                                     secondary_address="test secondary address", secondary_phone="test secondary phone", secondary_notes="test secondary notes"))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,12 +11,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="test group name", header="test group header", footer="test group footer"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
