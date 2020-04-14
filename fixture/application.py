bfeb7c44a34a14483c0106e3d55308cec4a11564
Yevhen Hornysh <e.gornysh@gmail.com>
2020-04-14 12:24:20 +0300

    created class ContactHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -10,49 +11,15 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook")
 
     def return_to_homepage(self):
         wd = self.wd
         wd.find_element_by_link_text("HOME").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        self.open_add_contact_page()
-        # fill contact form
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").send_keys(contact.phone_home)
-        wd.find_element_by_name("work").send_keys(contact.phone_work)
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        wd.find_element_by_xpath(contact.birthday_day).click()
-        wd.find_element_by_xpath(contact.birthday_month).click()
-        wd.find_element_by_name("byear").send_keys(contact.birthday_year)
-        wd.find_element_by_xpath(contact.anniversary_day).click()
-        wd.find_element_by_xpath(contact.anniversary_month).click()
-        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
-        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
-        wd.find_element_by_name("phone2").send_keys(contact.secondary_phone)
-        wd.find_element_by_name("notes").send_keys(contact.secondary_notes)
-        # submit contact creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_homepage()
-
-    def open_add_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("ADD_NEW").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="test fname", middle_name="test mname", last_name="test lname", nickname="test nickname",
+    app.contact.create_contact(Contact(first_name="test fname", middle_name="test mname", last_name="test lname", nickname="test nickname",
                                     title="test title", company="test company", address="test address",
                                     phone_home="test phone home", phone_mobile="test phone mobile", phone_work="test phone work", fax="test fax",
                                     email="test email", email2="test email 2", email3="test email 3", homepage="test homepage",
