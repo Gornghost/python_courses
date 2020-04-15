16fd5189ba2517083060b6862e18bf4547912b04
Yevhen Hornysh <e.gornysh@gmail.com>
2020-04-15 17:35:16 +0300

    implemented smart login and logout

--- fixture/session.py
+++ fixture/session.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+
 class SessionHelper:
 
     def __init__(self, app):
@@ -15,3 +16,25 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("LOGOUT").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("LOGOUT")) > 0
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//form[@name='logout']/b").text == "(" + username + ")"
