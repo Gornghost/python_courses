5141498458f19a9e772a72a88e72fb6476969e52
Yevhen Hornysh <e.gornysh@gmail.com>
2020-04-19 15:32:24 +0300

    added assertion for "delete group" test

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,10 +4,16 @@ from model.group import Group
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="name EDITED"))
+    new_group = app.group.get_group_list()
+    assert len(old_groups) == len(new_group)
 
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="header EDITED"))
+    new_group = app.group.get_group_list()
+    assert len(old_groups) == len(new_group)
