756d77caba67f71b25de1076ccb80d9dd45d8206
Yevhen Hornysh <e.gornysh@gmail.com>
2020-04-29 15:51:23 +0300

    sorting and comapring groups for add_group and edit_group tests

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
 
     def __init__(self, name=None, header=None, footer=None, group_id=None):
@@ -7,7 +10,13 @@ class Group:
         self.group_id = group_id
 
     def __eq__(self, other):
-        return self.name == other.name and self.group_id == other.group_id
+        return self.name == other.name and (self.group_id is None or other.group_id is None or self.group_id == other.group_id)
 
     def __repr__(self):
         return "%s:%s" % (self.group_id, self.name)
+
+    def id_or_max(self):
+        if self.group_id:
+            return int(self.group_id)
+        else:
+            return maxsize
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,19 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="test group name", header="test group header", footer="test group footer"))
-    new_group = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_group)
+    group = Group(name="test group name", header="test group header", footer="test group footer")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_group = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_group)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_group, key=Group.id_or_max)
