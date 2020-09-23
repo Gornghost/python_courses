86c5cd097a57a28d8b76337dbc87e639c1def18b
Yevhen Hornysh <e.gornysh@gmail.com>
2020-09-23 14:13:57 +0300

    added adding options from the console

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,12 +7,24 @@ from random import randrange
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        elif browser == "opera":
+            self.wd = webdriver.Opera()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +36,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("MainForm")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def return_to_homepage(self):
         wd = self.wd
